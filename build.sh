9f2b81e4cb4b9f0761583d98a6a039e139cd5891
Yannis Becker <yanbec2000@gmail.com>
2022-04-05 12:26:55 +0200

    basic linux compile script

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if ! command -v sdl2-config &> /dev/null
+then
+    apt install libsdl2-dev libsdl2-2.0-0 libjpeg-dev libwebp-dev libtiff5-dev libsdl2-image-dev libsdl2-image-2.0-0 -y
+fi
+
+gcc `sdl2-config --cflags --libs` -lm -o build/turtle src/sdl/sdlinterf.c src/main.c src/eval.c src/parse.c src/lex.c src/nametab.c -lSDL2main -lSDL2 -Wl,--no-undefined -lm -luuid -static-libgcc
